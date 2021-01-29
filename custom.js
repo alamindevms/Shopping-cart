24279c309951ae2771258e2e19d54d14f804070a
Al Amin Hossain <alamin.mistrisolutions@gmail.com>
2021-01-29 09:49:21 +0600

    all button working by one function

--- custom.js
+++ custom.js
@@ -1,62 +1,23 @@
-// Phone
-const addBtn = document.getElementById('add');
-const removeBtn = document.getElementById('remove');
-// Case 
-let caseRemove = document.getElementById('case-remove');
-let caseAdd = document.getElementById('case-add');
 
-caseAdd.addEventListener('click', caseAddFunc);
-caseRemove.addEventListener('click', caseRemoveFunc);
-// eventListener
-addBtn.addEventListener('click', addPrice);
-removeBtn.addEventListener('click', removeBtnPrice);
 // function 
-function addPrice() {
-   handleProductPrice(true);
-}
+function handleProductPrice(product, isIncrease) {
+   let productInput = document.getElementById(product + '-count');
+   let productValue = parseInt(productInput.value);
 
-function removeBtnPrice() {
-   handleProductPrice(false);
-}
-
-function handleProductPrice(isIncrease) {
-   let price = document.getElementById('price');
-   let priceValue = parseInt(price.value);
-
-   let newValue = priceValue;
+   let newValue = productValue;
    if (isIncrease == true) {
-      newValue = priceValue + 1;
+      newValue = productValue + 1;
    }
-   if (isIncrease == false && priceValue > 0) {
-      newValue = priceValue - 1;
+   if (isIncrease == false && productValue > 0) {
+      newValue = productValue - 1;
    }
-   price.value = newValue;
-   let totalPrice = newValue * 1219;
-   document.getElementById('totalPrice').innerText = '$' + totalPrice;
-}
-
-
-
-function caseAddFunc() {
-   caseHandaler(true)
-}
-
-function caseRemoveFunc() {
-   caseHandaler(false)
-}
-
-function caseHandaler(isIncrease) {
-   let caseInput = document.getElementById('case-input');
-   let caseValue = parseInt(caseInput.value);
-   let updateValue = caseValue;
-   if (isIncrease == true) {
-      updateValue = caseValue + 1;
+   productInput.value = newValue;
+   let totalPrice = 0;
+   if (product == 'phone') {
+      totalPrice = newValue * 1219;
    }
-   if (isIncrease == false && caseValue > 0) {
-      updateValue = caseValue - 1;
+   if (product == 'case') {
+      totalPrice = newValue * 59;
    }
-   caseInput.value = updateValue;
-
-   let caseTotal = updateValue * 59;
-   document.getElementById('case-total').innerText = '$' + caseTotal;
+   document.getElementById(product + '-total').innerText = '$' + totalPrice;
 }
